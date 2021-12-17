19d32e30627836de3737d5de08c58b8055c24bb6
odzen <jsebastian.va@gmail.com>
2021-12-16 19:44:48 -0500

    Update

---
+++ tareaConFork_Exec/1_cincoComandosFork/variant2/five2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void) {
+	pid_t bash = getppid();//PID de la terminal
+	pid_t pmain = getpid();//PID del proceso main
+	printf("proceso padre[%d] - proceso main[%d]\n",bash,pmain);
+	int i=0;
+	pid_t rc; // el PID para el fork
+	while(i<5){ 
+		rc = fork(); // Se crea una copia del proceso
+		if(rc<0){ // cuando no hay proceso
+			fprintf(stderr,"No se creó el proceso\n");
+			exit(1);
+		}else if(rc==0){
+			//Codigo ejecutado por cada proceso hijo
+			printf("El hijo[%d] - El padre es[%d]\n",getpid(), pmain); // Se imprime el padre y el hijo con el PID
+			return 0;
+		}else{
+			wait(NULL);
+		}
+		i++;
+	}
+  return 0;
+}
