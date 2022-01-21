cc0294cb8d42b0818ccdb84d9849a6768c61a83b
odzen <jsebastian.va@gmail.com>
2022-01-20 19:12:50 -0500

    Add point 1 and 2, working with pipes

---
+++ tareaPipes/pipe-punto2.c
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define READ  0 // Entrada estandar de un proceso valor 0
+#define WRITE 1 // Salida estandar de un proceso valor 1
+
+int main() {
+
+  pid_t pid; // Guarda lo que devuelve la llamada al sistema fork
+  int   fd[2]; // Guarda lo que devuelve la llamada al sistema pipe
+               // Posicion 0: Entrada estandar. Posicion 1: Salida estandar
+ 
+  if (pipe(fd) == -1) { //Si el valor que se devuelve es -1
+	perror("Creating pipe"); // es un error
+	exit(EXIT_FAILURE);  // Sale de la ejecucion
+	}
+// Se continua si se vuelve un valor distinto a -1
+
+  switch(pid = fork()) { // Menu para llamar la funcion fork
+
+  case 0: // Si el valor que devuelve pid es 0
+          // El nuevo proceso ejecutara lo siguiente:
+
+	// The child process will execute wc.
+	// Close the pipe write descriptor.
+	close(fd[WRITE]); // Interesa leer datos
+
+	// Redirect STDIN to read from the pipe.
+	dup2(fd[READ], STDIN_FILENO);
+
+	// Execute sort
+	// reemplaza el codigo del proceso por sort
+	 execvp("sort", "sort", NULL);
+
+  case -1: // Si el valor que devuelve pid es -1
+	perror("fork() failed)"); // es un error
+	exit(EXIT_FAILURE);// se sale de la ejecucion
+
+// si el pid es distinto a 0 y -1
+// se tiene el pid del proceso padre
+
+  default:
+	// The parent process will execute ls.
+	// Close the pipe read descriptor.
+	close(fd[READ]); // Interesa escribir datos
+
+	// Redirect STDOUT to write to the pipe.
+	dup2(fd[WRITE], STDOUT_FILENO);
+
+    // Execute cat /etc/hosts
+	// reemplaza el codigo del proceso por "cat /etc/hosts"
+	 execvp("cat /etc/hosts", "cat /etc/hosts", NULL);
+  }
+}
