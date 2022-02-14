8bf15e6942373d8c0e4a1e38952bd4aac02758f2
odzen <jsebastian.va@gmail.com>
2022-02-14 13:10:42 -0500

    Finished practice with flask postgres, aprendi como crear tablas desde la app

--- flask_postgres/app.py
+++ flask_postgres/app.py
@@ -1,3 +1,4 @@
+from sqlalchemy import PrimaryKeyConstraint
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,11 +10,26 @@ app=Flask(__name__)
 #SQLALCHEMY_DATABASE_URI -> key value
 #Each database has a different config string
 #En el caso de postgres es: postgresql://postgres:password@localhost/flaskmovie
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/flaskmovie'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres2819@localhost/flaskmovie'
 
-##Crear tablas y esquema de la base de datos
+
 ##Creado db objects y pasar la app
 db=SQLAlchemy(app)
+
+##Crear tablas y esquema de la base de datos
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80),unique=True)
+    email=db.Column(db.String(120), unique=True)
+
+    def __init__(slef,username,email):
+        self.username=username
+        self.email=email
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/')
 
