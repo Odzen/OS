ab863d257042929e2e58300d54d5091b5925c928
odzen <jsebastian.va@gmail.com>
2022-02-27 23:31:24 -0500

    Edit config.py

--- Docker/tareaDockerVolumeNetwork/config.py
+++ Docker/tareaDockerVolumeNetwork/config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
